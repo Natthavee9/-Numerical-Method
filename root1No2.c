8d5b1c630d10ee6cd940517bffbb9cf9092dfc5f
Natthavee9 <y.natthavee@gmail.com>
2025-08-03 09:56:22 +0700

    change condition

--- root1No2.c
+++ root1No2.c
@@ -7,37 +7,27 @@ double cal(double num){
 int main(){
        double xL = 1.5;
        double xR = 2.0;
-       double xM ,fxM,fxR,xM_old,xM_new,E;
+       double xM ,fxM,fxR,xM_old,xM_new,E=1.0;
        int iteration = 0;
        do{
               xM = (xL+xR)/2;
               fxM = cal(xM);
               fxR = cal(xR);
               xM_new = xM;
-              if(iteration == 0){
-                 if(fxM*fxR < 0){
-                     xL = xM;
-                 }
-                 else{
-                     xR = xM;
-                 }
-                 iteration++;
-                 continue;
-              }
               
-              if(iteration > 0){
-                     E = fabs((xM_new-xM_old)/xM_new);
-              }
-              xM_old = xM;
-
               if(fxM*fxR < 0){
                      xL = xM;
               }
               else{
                      xR = xM;
               }
+              
+              if(iteration > 0){
+                     E = fabs((xM_new-xM_old)/xM_new);
+              }
+              xM_old = xM;
               iteration++;
-       }while(iteration <=4);
+       }while(E > 0.000001);
        printf("ans = %.6lf\n",xM);
        printf("E = %.6lf\n",E);
        return 0;
